84881c07b16436760163ffe386015641efe4b38e
GaneshD15 <109506024+GaneshD15@users.noreply.github.com>
2022-09-29 09:56:56 +0530

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
 // import { StudentComponent } from './book/book.component';
 import { StudentComponent } from './student/student.component';
+import { ProductComponent } from './product/product.component';
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent, StudentComponent],
-  bootstrap: [StudentComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    StudentComponent,
+    ProductComponent,
+  ],
+  bootstrap: [StudentComponent, ProductComponent],
 })
 export class AppModule {}
